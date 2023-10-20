c05af6390a693e696a6245280e829852cd572082
jerry <demiso5@naver.com>
2023-10-20 15:28:56 +0900

    10월 22일 css 수정

--- src/components/Todo/Calendar.js
+++ src/components/Todo/Calendar.js
@@ -6,12 +6,15 @@ function ReactCalendar() {
   const [value, onChange] = useState(new Date());
 
   return (
-    <div>
+    <div id='calendar_container'>
       <Calendar
         onChange={onChange}
         value={value}
         minDetail='year'
         formatDay={(locale, date) => date.toLocaleString("en", { day: "numeric" })}
+        showNeighboringMonth={false}
+        minDate={new Date(2022, 1, 1)}
+        maxDate={new Date(2033, 12, 31)}
       />
     </div>
   );
--- src/components/Todo/Leftcontents.js
+++ src/components/Todo/Leftcontents.js
@@ -10,7 +10,9 @@ import {
   faCheck,
   faX,
 } from "@fortawesome/free-solid-svg-icons";
+import Calendar from "react-calendar";
 import ReactCalendar from "./Calendar";
+// import ReactCalendar from "./Calendar";
 
 function LeftContents() {
   let [text, setText] = useState('');
@@ -58,7 +60,7 @@ function LeftContents() {
   return (
     <div id="todo_left_contents">
       <h1 className="todo_h1">To do List</h1>
-      <div className="todo_calendar"><ReactCalendar /></div>
+      <ReactCalendar />
       <div className="todo_checkLists">
         {todoList.map((item, i) => {
           console.log(todoList)
