df497ecc0b179948813c5658c6aa6d2187d2f273
jerry <demiso5@naver.com>
2023-10-20 16:30:39 +0900

    10월 20일 css 수정본

--- src/components/Todo/Calendar.js
+++ src/components/Todo/Calendar.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import Calendar from 'react-calendar';
-import '../../Pages/styles/Calendar_custom.css'; // css import
-
-function ReactCalendar() {
-  const [value, onChange] = useState(new Date());
-
-  return (
-    <div>
-      <Calendar
-        onChange={onChange}
-        value={value}
-        minDetail='year'
-        formatDay={(locale, date) => date.toLocaleString("en", { day: "numeric" })}
-      />
-    </div>
-  );
-}
-
-=======
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import '../../Pages/styles/Calendar_custom.css'; // css import
@@ -41,5 +20,4 @@ function ReactCalendar() {
   );
 }
 
->>>>>>> 7bbbe6c4c0d561b562b3114b544d17e7862c796b
 export default ReactCalendar; 
