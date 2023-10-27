e9997dd75e8e676cb6ba7896617f42f6f46ba533
leejink0 <ljkangle@naver.com>
2023-10-27 23:00:28 +0900

    main page carousel & 미디어 쿼리 css 작업

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -7,6 +7,7 @@
 -로그
 231016 김장훈 - 최초작성
 231020 이진경 - 수정
+231027 이진경 - 캐러셀 추가
 */
 
 import React, { useState, useEffect } from "react";
@@ -15,6 +16,10 @@ import PopularRank from "../components/Community/PopularRank";
 import getGroupData from "../components/Community/getGroupData";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBullhorn, faComments } from "@fortawesome/free-solid-svg-icons";
+// 배너
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 function Main() {
   const [groupData, setGroupData] = useState([]);
@@ -27,21 +32,77 @@ function Main() {
     fetchGroupData();
   }, []);
 
+  // 캐러셀 세팅값
+  const settings = {
+    dots: true,
+    fade: true,
+    infinite: true,
+    speed: 1500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: true,
+    arrow: false,
+  };
+
   return (
     <div className="main">
-      <div className="Hello">
-        <div className="image"></div>
-        <div className="title">
-          <div className="title_inner">
-            <h3>함께해서 즐거운 건강관리</h3>
-            <h1>Healthy Pleasure</h1>
-            <p>커뮤니티를 통해 크루들과 함께하는 즐겁고 지속적인 건강관리</p>
-            <div className="bt1">
-              <button>Show Now</button>
+      {/* 메인 배너 */}
+      <Slider {...settings} dotsClass="test-css">
+        <div className="banner">
+          <div className="banner_inner">
+            <div className="banner_image"></div>
+            <div className="banner_title">
+              <h3>Everyday! WE RUN!</h3>
+              <h1>Healthy Pleasure</h1>
+              <p>커뮤니티를 통해 크루들과 함께하는 즐겁고 지속적인 건강관리</p>
+              <div className="bt1">
+                <button>Show Now</button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+        <div className="banner2">
+          <div className="banner_inner">
+            <div className="banner_image"></div>
+            <div className="banner_title">
+              <h3>나만의 건강관리를 공유해보세요</h3>
+              <h1>Healthy Pleasure</h1>
+              <p>커뮤니티를 통해 크루들과 함께하는 즐겁고 지속적인 건강관리</p>
+              <div className="bt1">
+                <button>Show Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="banner3">
+          <div className="banner_inner">
+            <div className="banner_image"></div>
+            <div className="banner_title">
+              <h3>오늘도 다함께 GO! GO!</h3>
+              <h1>Healthy Pleasure</h1>
+              <p>커뮤니티를 통해 크루들과 함께하는 즐겁고 지속적인 건강관리</p>
+              <div className="bt1">
+                <button>Show Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="banner4">
+          <div className="banner_inner">
+            <div className="banner_image"></div>
+            <div className="banner_title">
+              <h3>목표 달성을 위한 커뮤니티</h3>
+              <h1>Healthy Pleasure</h1>
+              <p>커뮤니티를 통해 크루들과 함께하는 즐겁고 지속적인 건강관리</p>
+              <div className="bt1">
+                <button>Show Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Slider>
 
       <div className="rank">
         <div className="title">
