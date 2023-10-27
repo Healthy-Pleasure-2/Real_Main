53d2fa27a05bdb6dd04edc6a86e1b52d9479a144
jerry <demiso5@naver.com>
2023-10-27 17:43:57 +0900

    캘린더 UI 수정(요일 영어로, 달력 크기 조정)

--- src/components/Todo/Calendar.js
+++ src/components/Todo/Calendar.js
@@ -15,6 +15,14 @@ function ReactCalendar() {
         showNeighboringMonth={false}
         minDate={new Date(2021, 12, 31)}
         maxDate={new Date(2033, 11, 31)}
+        formatShortWeekday={(locale, date) => {
+          // date 객체의 getDay 메서드를 사용하여 해당 날짜의 요일을 가져옵니다.
+          const dayOfWeek = date.getDay();
+
+          // 요일을 영어로 변환합니다.
+          const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+          return daysOfWeek[dayOfWeek];
+        }}
       />
     </div>
   );
