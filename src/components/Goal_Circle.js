74646c4b3b8b69cfa0a216288513f7b254fa1c72
Jang Hun Kim <son641124@naver.com>
2023-10-31 20:22:41 +0900

    Goal_Circle 목표값에 따른 최대값 변경 수정완료

--- src/components/Goal_Circle.js
+++ src/components/Goal_Circle.js
@@ -10,6 +10,8 @@
 231017 김장훈 - 입력창 및 입력버튼을 통한 시각화 변화하도록 수정. UI다듬기 등 추가작업 필요
 231018 김장훈 - 입력버튼 삭제(input 박스에 입력되면 시각화 바로 변화되도록 수정), UI 다듬기, 차후 Todo 페이지에서 최대값 불러오기 추가예정 
 231029 김장훈 - 쿠키에서 사용자정보 불러오기 코드 작성중.
+231030 김장훈 - 가져와지는 목표값 최대값으로 지정하는것 수정
+231031 김장훈 - server.js 목표값 받아오는 기능 수정 (feat.은정)
 --------------------------------------------------------------------------------------------------------------
 *참고 사이트
 1. https://medium.com/@pppped/how-to-code-a-responsive-circular-percentage-chart-with-svg-and-css-3632f8cd7705 = 개념
@@ -21,31 +23,47 @@ import React, { useState, useEffect } from "react";
 import "./Goal_Circle.css";
 
 function Goal_Circle({ sessiondata }) {
+  //입력
   const [input_weight, setWeight] = useState("0");
   const [input_exercise, setExercise] = useState("0");
   const [input_diet, setDiet] = useState("0");
-
+  //목표
+  const [max_weight, setmaxWeight] = useState("0");
+  const [max_exercise, setmaxExercise] = useState("0");
+  const [max_diet, setmaxDiet] = useState("0");
+  //StrokeDasharray 설정
   const [weightStrokeDasharray, setWeightStrokeDasharray] = useState("0");
   const [exerciseStrokeDasharray, setExerciseStrokeDasharray] = useState("0");
   const [dietStrokeDasharray, setDietStrokeDasharray] = useState("0");
-  const [user_max, setUserdata] = useState("");
 
-    // 서버에서 사용자 데이터를 가져오는 네트워크 요청
-  useEffect(() => {
-    fetch("http://localhost:3003/Goal")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setUserdata(user_max.name);
-        setWeightStrokeDasharray(BaseValue_weight(parseFloat(data.weight)));
-        setExerciseStrokeDasharray(BaseValue_exercise(parseFloat(data.exercise)));
-        setDietStrokeDasharray(BaseValue_diet(parseFloat(data.diet)));
+  // 클라이언트에서 사용자 정보 가져오기
+  const fetchUserData = (userid) => {
+    fetch(`http://localhost:3003/user_Goal/${userid}`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("네트워크 에러");
+      })
+      .then((userData) => {
+        const { weight, exercise, diet } = userData;
+        setmaxWeight(weight);
+        setmaxExercise(exercise);
+        setmaxDiet(diet);
+      })
+      .catch((error) => {
+        console.error("에러:", error);
       });
-  }, []);
+  };
+  //초기 실행 시 필요
+  useEffect(() => {
+    fetchUserData(sessiondata);
+  }, [sessiondata]);
 
-//체중
+
+  //체중값 설정 및 계산
   const BaseValue_weight = (weightValue) => {
-    const maxWeight = 1000;
+    const maxWeight = max_weight;
     const minWeight = 0;
     const maxWeightStrokeDasharray = 100;
     const minWeightStrokeDasharray = 0;
@@ -57,9 +75,9 @@ function Goal_Circle({ sessiondata }) {
       : ((weightValue - minWeight) / (maxWeight - minWeight)) * maxWeightStrokeDasharray;
   };
 
-  //운동
+  //운동값 설정 및 계산
   const BaseValue_exercise = (exerciseValue) => {
-    const maxExercise = 1000;
+    const maxExercise = max_exercise;
     const minExercise = 0;
     const maxExerciseStrokeDasharray = 100;
     const minExerciseStrokeDasharray = 0;
@@ -68,12 +86,12 @@ function Goal_Circle({ sessiondata }) {
       ? maxExerciseStrokeDasharray
       : exerciseValue <= minExercise
       ? minExerciseStrokeDasharray
-      : ((exerciseValue - minExercise) / (maxExercise - minExercise)) *maxExerciseStrokeDasharray;
+      : ((exerciseValue - minExercise) / (maxExercise - minExercise)) * maxExerciseStrokeDasharray;
   };
 
-  //식단
+  //식단값 설정 및 계산
   const BaseValue_diet = (dietValue) => {
-    const maxDiet = 1000;
+    const maxDiet = max_diet;
     const minDiet = 0;
     const maxDietStrokeDasharray = 100;
     const minDietStrokeDasharray = 0;
@@ -82,9 +100,10 @@ function Goal_Circle({ sessiondata }) {
       ? maxDietStrokeDasharray
       : dietValue <= minDiet
       ? minDietStrokeDasharray
-      : ((dietValue - minDiet) / (maxDiet - minDiet)) *maxDietStrokeDasharray;
+      : ((dietValue - minDiet) / (maxDiet - minDiet)) * maxDietStrokeDasharray;
   };
 
+  //사용자 입력값 시각화처리
   const handleInputChange = (inputType, inputValue) => {
     if (inputType === "input_weight") {
       setWeight(inputValue);
@@ -176,51 +195,3 @@ function Goal_Circle({ sessiondata }) {
   );
 }
 export default Goal_Circle;
-
-/*초안*/
-/*
-import React from "react";
-import "./Goal_Circle.css";
-function Goal_Circle() {
-  return (
-    <div className="flex-wrapper">
-      <div className="single-chart">
-        <svg viewBox="0 0 36 36" className="circular-chart orange">
-          <path
-            className="circle"
-            strokeDasharray="90, 100"
-            d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"
-          />
-          <text x="10.2" y="15" fontSize="2.5px" className="percentage">
-            체중:___kg
-          </text>
-        </svg>
-      </div>
-      <div className="single-chart1">
-        <svg viewBox="0 0 36 36" className="circular-chart1 green">
-          <path
-            className="circle1"
-            strokeDasharray="90, 100"
-            d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"
-          />
-          <text x="8.5" y="18.8" fontSize="3px" className="percentage1">
-            운동:___kcal
-          </text>
-        </svg>
-      </div>
-      <div className="single-chart2">
-        <svg viewBox="0 0 36 36" className="circular-chart2 blue">
-          <path
-            className="circle2"
-            strokeDasharray="90, 100"
-            d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"
-          />
-          <text x="7" y="25" fontSize="3.5px" className="percentage2">
-            식단:___kcal
-          </text>
-        </svg>
-      </div>
-    </div>
-  );
-}
-export default Goal_Circle; */
